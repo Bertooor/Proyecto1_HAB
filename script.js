a9e228a06a92463f5d8a6fa30aca7c200b72aef5
Alberto <albertoleandro37@gmail.com>
2022-06-20 19:14:25 +0200

    opcion con async

--- script.js
+++ script.js
@@ -1,70 +1,42 @@
 "use strict";
 
 const weather_key = '6dcfbe4f5d769224af0c28453d74d3e7';
-//const forecast_key = '50b4400f89634c70845a4b0d583a9986';
 
 const button = document.querySelector("button");
 
-//const urlWeather = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${weather_key}`;
-
-const user = position => {
-    const { latitude, longitude } = position.coords;
-    //fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weather_key}`)
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${weather_key}`)
-    .then(response => response.json())
-    //.then(data => console.log(data))
-    .then(data => setWeatherRain(data))
-    // async function getWeather(url) {
-    //     try {
-    //         let response = await fetch(url);
-    //         let data = await response.json(response);
-    //     } catch(error) {
-
-    //     }
-    // } 
-}
-
-const setWeatherRain = data => {
-    const li1 = document.getElementById("3hours");
-    const li2 = document.getElementById("rain1");
-    const li3 = document.getElementById("6hours");
-    const li4 = document.getElementById("rain2");
-    const li5 = document.getElementById("9hours");
-    const li6 = document.getElementById("rain3");
-    const li7 = document.getElementById("12hours");
-    const li8 = document.getElementById("rain4");
-    console.log(data);
 
 
-    //Falta gestionar los undefined de los periodos en los que no llueve.
-//     li2.textContent += `Lluvia: ${data.list[0].rain["3h"]} mm/3h`;  
-//     li4.textContent += `Lluvia: ${data.list[1].rain["3h"]} mm/3h`; 
-//     li6.textContent += `Lluvia: ${data.list[2].rain["3h"]} mm/3h`; 
-//     li8.textContent += `Lluvia: ${data.list[3].rain["3h"]} mm/3h`; 
-//     li1.textContent += data.list[0].dt_txt;
-//     li3.textContent += data.list[1].dt_txt;
-//     li5.textContent += data.list[2].dt_txt;
-//     li7.textContent += data.list[3].dt_txt;
-// 
+
+const li1 = document.getElementById("3hours");
+const li2 = document.getElementById("rain1");
+const li3 = document.getElementById("6hours");
+const li4 = document.getElementById("rain2");
+const li5 = document.getElementById("9hours");
+const li6 = document.getElementById("rain3");
+const li7 = document.getElementById("12hours");
+const li8 = document.getElementById("rain4");
 
 
-    //Gestionando los undefined con weather en lugar de rain
-    li2.textContent += `${data.list[0].weather[0].main} / ${data.list[0].weather[0].description} `;
-    li4.textContent += `${data.list[1].weather[0].main} / ${data.list[1].weather[0].description} `;
-    li6.textContent += `${data.list[2].weather[0].main} / ${data.list[2].weather[0].description} `;
-    li8.textContent += `${data.list[3].weather[0].main} / ${data.list[3].weather[0].description} `;
-    li1.textContent += data.list[0].dt_txt;
-    li3.textContent += data.list[1].dt_txt;
-    li5.textContent += data.list[2].dt_txt;
-    li7.textContent += data.list[3].dt_txt;
-}
+async function user (position) {
+    try {
+            const { latitude, longitude } = position.coords;
+            let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${weather_key}`);
+            let data = await response.json(response);
+            console.log(data);
+            li2.textContent += `${data.list[0].weather[0].main} / ${data.list[0].weather[0].description} `;
+            li4.textContent += `${data.list[1].weather[0].main} / ${data.list[1].weather[0].description} `;
+            li6.textContent += `${data.list[2].weather[0].main} / ${data.list[2].weather[0].description} `;
+            li8.textContent += `${data.list[3].weather[0].main} / ${data.list[3].weather[0].description} `;
+            li1.textContent += data.list[0].dt_txt;
+            li3.textContent += data.list[1].dt_txt;
+            li5.textContent += data.list[2].dt_txt;
+            li7.textContent += data.list[3].dt_txt;
+        } catch(error) {
+            console.error(`tenemos un error`, error.message);
+        }
+} 
+user();
 
-//Hay que resetear el button
 button.addEventListener("click", () => {
     navigator.geolocation.getCurrentPosition(user);
 })
-
-//`Lluvia: ${data.list[0].rain["3h"]} mm/3h`
-
-//El ternario no funciona, al parecer la promesa no puede leer undefined y da error.
-//data.list[0].rain["3h"] === undefined ? 'No llueve' : `Lluvia: ${data.list[0].rain["3h"]} mm/3h`
