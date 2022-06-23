9aafd1dfb0ad8d5912cd48b751e50296628970c8
Alberto <albertoleandro37@gmail.com>
2022-06-24 01:48:46 +0200

    cambiando aspecto

--- script.js
+++ script.js
@@ -3,43 +3,62 @@ const weather_key = '6dcfbe4f5d769224af0c28453d74d3e7';
 
 const button = document.querySelector("button");
 
-const li1 = document.getElementById("3hours");
-const li2 = document.getElementById("rain1");
-const li3 = document.getElementById("6hours");
-const li4 = document.getElementById("rain2");
-const li5 = document.getElementById("9hours");
-const li6 = document.getElementById("rain3");
-const li7 = document.getElementById("12hours");
-const li8 = document.getElementById("rain4");
-const li9 = document.getElementById("country");
-const li10 = document.getElementById("city");
-const li11 = document.getElementById("coord");
-const li12 = document.getElementById("population");
+
+const li1 = document.getElementById("city");
+const li2 = document.getElementById("hours3");
+const li3 = document.getElementById("temp1");
+const li4 = document.getElementById("icono1");
+const li5 = document.getElementById("rain1");
+const li6 = document.getElementById("hours6");
+const li7 = document.getElementById("temp2");
+const li8 = document.getElementById("icono2");
+const li9 = document.getElementById("rain2");
+const li10 = document.getElementById("hours9");
+const li11 = document.getElementById("temp3");
+const li12 = document.getElementById("icono3");
+const li13 = document.getElementById("rain3");
+const li14 = document.getElementById("hours12");
+const li15 = document.getElementById("temp4");
+const li16 = document.getElementById("icono4");
+const li17 = document.getElementById("rain4");
 
 
 async function user (position) {
     try {
-            const { latitude, longitude } = position.coords;
-            let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${weather_key}`);
-            let data = await response.json(response);
-            console.log(data);
-            li2.textContent = `${data.list[0].weather[0].main} / ${data.list[0].weather[0].description} `;
-            li4.textContent = `${data.list[1].weather[0].main} / ${data.list[1].weather[0].description} `;
-            li6.textContent = `${data.list[2].weather[0].main} / ${data.list[2].weather[0].description} `;
-            li8.textContent = `${data.list[3].weather[0].main} / ${data.list[3].weather[0].description} `;
-            li1.textContent = data.list[0].dt_txt;
-            li3.textContent = data.list[1].dt_txt;
-            li5.textContent = data.list[2].dt_txt;
-            li7.textContent = data.list[3].dt_txt;
-            li9.textContent = `Your country code is: ${data.city.country}`;
-            li10.textContent = `The city you are in is named: ${data.city.name}`;
-            li11.textContent = `Your coordinates are: Latitude ${latitude} and Longitude ${longitude}`;
-            li12.textContent = `The current population in ${data.city.name} is estimated to be of ${data.city.population} inhabitants`;
-        } catch(error) {
-            console.error(`tenemos un error`, error.message);
-        }
+        const { latitude, longitude } = position.coords;
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${weather_key}`);
+        let data = await response.json(response);
+        console.log(data);
+        li1.textContent = `${data.city.name}, ${data.city.country}`;
+        li2.textContent = data.list[0].dt_txt;
+        li3.textContent = `${toCelsius(data.list[0].main.temp)}c`;
+        //li4.textContent =
+        li5.textContent = data.list[0].weather[0].main;
+        li6.textContent = data.list[1].dt_txt;
+        li7.textContent = `${toCelsius(data.list[1].main.temp)}c`;
+        //li8.textContent =
+        li9.textContent = data.list[1].weather[0].main;
+        li10.textContent = data.list[2].dt_txt;
+        li11.textContent = `${toCelsius(data.list[2].main.temp)}c`;
+        //li12.textContent = 
+        li13.textContent = data.list[2].weather[0].main;
+        li14.textContent = data.list[3].dt_txt;
+        li15.textContent = `${toCelsius(data.list[3].main.temp)}c`;
+        //li16.textContent = 
+        li17.textContent = data.list[3].weather[0].main;
+    } catch(error) {
+        console.error(`tenemos un error`, error.message);
+    }
 } 
+
+function toCelsius (kelvin) {
+    return Math.round(kelvin - 273.15);
+}
 
 button.addEventListener("click", () => {
     navigator.geolocation.getCurrentPosition(user);
 });
+
+            
+            
+            
